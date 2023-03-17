3b87ddf44801a52a8a5c8270544aeb727a85d856
nadaissa <nada-issa_student2023@wilder.school>
2023-03-17 19:27:24 +0100

    upload quest initial solution done

--- form.php
+++ form.php
@@ -1,64 +1,61 @@
 <?php
 
-if(!empty($_FILES['files']['name'][0]))
-{
+if($_SERVER["REQUEST_METHOD"] === "POST" ){ 
+    
+    $uploadDir = 'uploads/';
+    
+    $extension = pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION);
+    $fileUniqueName = uniqid('', true) . '.' . $extension;
 
-  $files = $_FILES['files'];
+    $uploadFile = $uploadDir . basename($fileUniqueName);
 
-  $uploaded = array();
-  $failed = array();
+    $authorizedExtensions = ['jpg','gif','png', 'webp'];
+    $maxFileSize = 1000000;
 
-  $allowed = array('jpg', 'gif', 'webp');
+    $errors = [];
 
-  foreach($files['name'] as $position => $file_name)
-  {
-    $file_tmp = $files['tmp_name'][$position];
-    $file_size = $files['size'][$position];
-    $file_error = $files['error'][$position];
-  
-    $file_ext = explode('.', $file_name);
-    $file_ext = strtolower(end($file_ext));
-
-    if(in_array($file_ext, $allowed)){
-        if($file_error === 0){
-            if($file_size <= 131072){
-                
-                $file_name_new = uniqid('', true) . '.' . $file_ext;
-                $file_destination = 'uploads/' . $file_name_new;
-                
-                if(move_uploaded_file($file_tmp, $file_destination)){
-                        $uploaded[$position] = $file_destination;
-
-                }else{
-                    $failed[$position] = "[{$file_name}] failed to upload";
-                }
-
-            }else{
-                $failed[$position] = "[{$file_name}] is too large";
-            }
-
-        }else{
-            $failed[$position] = "[{$file_name}] errored with code {$file_error}";
-        }
-
-    }else{
-        $failed[$position] = "[{$file_name}] file extension '{$file_ext}' is not allowed ";
+    if( (!in_array($extension, $authorizedExtensions))){
+        $errors[] = 'Veuillez sélectionner une image de type Jpg ou Gif ou Png ou Webp !';
+    }
+    if(file_exists($_FILES['image']['tmp_name']) && filesize($_FILES['image']['tmp_name']) > $maxFileSize)
+    {
+    $errors[] = "Votre fichier doit faire moins de 1M !";
     }
 
-
+    if (!empty($errors)) {
+        foreach($errors as $error){
+            echo $error;
+        }
+     } else {
+        move_uploaded_file($_FILES['image']['tmp_name'], $uploadFile);
+     }  
     
-
-        if(!empty($uploaded)){
-            var_dump($uploaded);
-        }
-
-        if(!empty($failed)){
-            var_dump($failed);
-        }
-
-
-  }
 }
 
-var_dump($_FILES);
+// var_dump($_FILES);
+// var_dump($_POST);
+
 ?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Upload quest</title>
+</head>
+<body>
+
+
+<form action="form.php" method="post" enctype="multipart/form-data">
+    <label for="image">Votre image</label>
+    <input type="file" name="image">
+    <button type="submit">Soumettre</button>
+
+</form>
+
+
+</body>
+</html>
